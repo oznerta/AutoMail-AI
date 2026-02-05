81f3995a83062e7d25ae8cdedfa9fef25c17fdfe
Matt Renzo C. Baring <mattrenzocabagnotbaring@gmail.com>
2026-02-06 00:37:15 +0800

    favicon

---
+++ app/icon.tsx
@@ -0,0 +1,56 @@
+import { ImageResponse } from 'next/og'
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const size = {
+    width: 32,
+    height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 24,
+                    background: 'transparent', // Transparent background
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'hsl(173, 80%, 40%)', // Primary Color from globals.css
+                }}
+            >
+                {/* SVG from components/ui/logo.tsx */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    width="24"
+                    height="24"
+                >
+                    <rect x="2" y="4" width="20" height="16" rx="2" />
+                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+                    <path d="M12 22v-6" />
+                    <path d="M8 22h8" />
+                </svg>
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported icons size metadata
+            // config to also set the ImageResponse's width and height.
+            ...size,
+        }
+    )
+}
